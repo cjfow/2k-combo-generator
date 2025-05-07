d02077baef647cb4bc1d0c9276d33bbb3ac4cb67
cjfow <cfowler1219@gmail.com>
2025-05-07 19:58:44 -0400

    Add project files.

---
+++ combo-generator-library/Move.cs
@@ -0,0 +1,28 @@
+﻿namespace combo_generator_library;
+
+public enum Direction
+{
+    FacingHoop,
+    FacingAway,
+    SidewaysBallToBasket,
+    SidewaysBallAway
+}
+
+public enum Position
+{
+    Standing,
+    Walking,
+    Running
+}
+
+public enum Difficulty
+{
+    Easy,
+    Medium,
+    Hard
+}
+
+public class Move
+{
+
+}
